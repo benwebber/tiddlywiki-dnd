28d009d9fc7e5909f418492d0d468b948a043571
Ben Webber <benjamin.webber@gmail.com>
2018-08-31 08:59:04 +0000

    Remove outstanding module metadata

--- src/modules/macros/dnd.average.js
+++ src/modules/macros/dnd.average.js
@@ -1,10 +1,3 @@
-/*\
-title: $:/plugins/benwebber/dnd/macros/average.js
-type: application/javascript
-module-type: macro
-
-Returns the average value for a specified roll.
-\*/
 import {average} from "$:/plugins/benwebber/dnd/dnd.js";
 
 export const name = "dnd.average";
