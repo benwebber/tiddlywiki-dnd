8e800901c6d1f74ea20e0f3363d989c93527debc
Ben Webber <benjamin.webber@gmail.com>
2019-01-14 06:00:35 +0000

    Evaluate WikiText in localized strings

--- src/modules/macros/dnd.lingo.ts
+++ src/modules/macros/dnd.lingo.ts
@@ -1,13 +1,5 @@
 declare const $tw: any;
 
-function getPluginString(widget: any, plugin: string, languageTitle: string, title: string, options = {}) {
-  return widget.wiki.renderTiddler(
-    "text/plain",
-    `$:/plugins/${plugin}/languages/${languageTitle}/${title}`,
-    options,
-  );
-}
-
 export const name = "dnd.lingo";
 export const params = [
   {name: "title"},
@@ -15,21 +7,20 @@ export const params = [
 export function run(title) {
   const plugin = "benwebber/dnd";
   const languageTitle = this.getVariable("languageTitle");
-  // Check if localized string exists.
-  let text = getPluginString(this, "benwebber/dnd", languageTitle, title);
-  if (text) {
-    return text;
+  const tiddlerTitle = `$:/plugins/${plugin}/languages/${languageTitle}/${title}`;
+  if (this.wiki.isShadowTiddler(tiddlerTitle)) {
+    return `{{${tiddlerTitle}}}`;
   }
   // Check if string is defined in dependency.
   const dependents = this.wiki.getTextReference(`$:/languages/${languageTitle}!!dependents`, "");
   const dependentLanguageTitles = $tw.utils.parseStringArray(dependents)
     .map((tiddler) => tiddler.replace("$:/languages/", ""));
   for (const dependentLanguageTitle of dependentLanguageTitles) {
-    text = getPluginString(this, plugin, dependentLanguageTitle, title);
-    if (text) {
-      return text;
+    const dependentTiddlerTitle = `$:/plugins/${plugin}/languages/${dependentLanguageTitle}/${title}`;
+    if (this.wiki.isShadowTiddler(dependentTiddlerTitle)) {
+      return `{{${dependentTiddlerTitle}}}`;
     }
   }
   // Fall back to default string.
-  return this.wiki.renderTiddler("text/plain", `$:/plugins/${plugin}/language/${title}`);
+  return `{{$:/plugins/${plugin}/language/${title}}}`;
 }
