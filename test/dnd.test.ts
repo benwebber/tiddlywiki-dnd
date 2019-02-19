000341032340fa24068b99d18bd6920e7688a142
Ben Webber <benjamin.webber@gmail.com>
2019-02-19 07:38:33 +0000

    Port dnd.ability tests to integration test harness

--- test/dnd.test.ts
+++ test/dnd.test.ts
@@ -22,22 +22,6 @@ describe("StatBlock", () => {
 });
 
 
-describe("ability", () => {
-  [
-    {score: 1, expected: "1 (−5)"},
-    {score: 5, expected: "5 (−3)"},
-    {score: 10, expected:  "10 (+0)"},
-    {score: 15, expected:  "15 (+2)"},
-    {score: 20, expected:  "20 (+5)"},
-    {score: 30, expected:  "30 (+10)"},
-  ].forEach((example) => {
-    it(`should return "${example.expected}" for score "${example.score}"`, () => {
-      expect(dnd.ability(example.score)).toBe(example.expected);
-    });
-  });
-});
-
-
 describe("capitalize", () => {
   [
     {description: "small beast, unaligned", expected: "Small beast, unaligned"},
--- test/macros.test.ts
+++ test/macros.test.ts
@@ -7,6 +7,8 @@ $tw.boot.argv = ["./build"];
 $tw.boot.boot();
 
 // Patch TiddlyWiki macros with source modules to measure coverage.
+import * as abilityMacro from "../src/modules/macros/dnd.ability";
+$tw.macros["dnd.ability"] = abilityMacro;
 import * as averageMacro from "../src/modules/macros/dnd.average";
 $tw.macros["dnd.average"] = averageMacro;
 
@@ -15,6 +17,22 @@ function checkOutput(wiki: any, wikitext: string, html: string) {
   const output = wiki.renderText("text/html", "text/vnd.tiddlywiki", wikitext);
   expect(output).toBe(html);
 }
+
+
+describe("dnd.ability", () => {
+  [
+    {score: 1, expected: "1 (−5)"},
+    {score: 5, expected: "5 (−3)"},
+    {score: 10, expected:  "10 (+0)"},
+    {score: 15, expected:  "15 (+2)"},
+    {score: 20, expected:  "20 (+5)"},
+    {score: 30, expected:  "30 (+10)"},
+  ].forEach((example) => {
+    it(`should return "${example.expected}" for score "${example.score}"`, () => {
+      checkOutput($tw.wiki, `<<dnd.ability ${example.score}>>`, `<p>${example.expected}</p>`);
+    });
+  });
+});
 
 
 describe("dnd.average", () => {
